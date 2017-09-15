a17f7da4e5e6682c62360aaef39f6170b304781e
Kuan-Wen Liu <j70915@gmail.com>
2017-09-15 23:01:19 +0800

    change fail/pass img name and js

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -247,7 +247,7 @@ myApp.onPageInit('challenge', function(page) {
         <circle class="path circle" fill="none" stroke="white" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>
         <polyline class="path check" fill="none" stroke="white" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>
       </svg>`);
-      result[number] = 'pass';
+      result[number] = 'PASS';
     } else {
       $$(`#${this.id}`).attr('style', 'background-image: url("img/btn-background/wrong-btn.png") !important');
       $$(`#${this.id}`).append(`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
@@ -255,7 +255,7 @@ myApp.onPageInit('challenge', function(page) {
         <line class="path line" fill="none" stroke="white" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="34.4" y1="37.9" x2="95.8" y2="92.3"/>
         <line class="path line" fill="none" stroke="white" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="95.8" y1="38" x2="34.4" y2="92.2"/>
       </svg>`);
-      result[number] = 'fail';
+      result[number] = 'Fail';
     }
 
     // wait for answer correct/wrong animate
